de5ab0ac02eb534e58f1618f82ca7798c3a9f409
retr0 err0r <retr0err0r@protonmail.com>
2024-10-09 19:59:56 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_requirements(file):
 
 setup(
     name='lib_clockifybot',
-    version='0.3',
+    version='0.4',
     author='retr0err0r - veininvein',
     packages=find_packages(),
     install_requires=read_requirements('requirements.txt'),
