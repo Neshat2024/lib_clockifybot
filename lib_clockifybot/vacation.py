ab7e2f7c04d74535a1eedf3cccb30aed48bdad44
arzshz <alirezashez@gmail.com>
2024-12-31 16:36:42 +0330

    vacation table updated - V1.7.1

--- lib_clockifybot/vacation.py
+++ lib_clockifybot/vacation.py
@@ -1,18 +1,18 @@
 import os
 
 from dotenv import load_dotenv
-from .log import add_log
-from .database import create_database_if_not_exists
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+from .database import create_database_if_not_exists
+from .log import add_log
 
 load_dotenv(os.getenv("CLOCKIFY_ENV"))
 
 VACATION_URL = os.getenv("DATABASE_URL_VACATION")
-engine_2 = create_engine(VACATION_URL)
+engine = create_engine(VACATION_URL)
 BaseVacation = declarative_base()
-SessionLocal_2 = sessionmaker(autocommit=False, autoflush=False, bind=engine_2)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class Vacation(BaseVacation):
@@ -20,12 +20,12 @@ class Vacation(BaseVacation):
     id = Column(Integer, primary_key=True, index=True)
     telegram_id = Column(String)
     username = Column(String)
+    coolname = Column(String)
     clockify_id = Column(String)
-    routine_shift = Column(String)
-    vdate = Column(String)
+    workday = Column(String)
     hours = Column(String)
-    confirmed_by = Column(String)
-
+    status = Column(String)
+    is_vacation = Column(Boolean)
     def __repr__(self):
         return f"User('{self.username}') - VacationDate('{self.vdate}' - RoutineVacation('{self.routine_vacation}'))"
 
@@ -33,6 +33,6 @@ class Vacation(BaseVacation):
 def init_vacation_db(bot):
     create_database_if_not_exists(VACATION_URL, bot)
     try:
-        BaseVacation.metadata.create_all(engine_2)
+        BaseVacation.metadata.create_all(engine)
     except Exception as e:
         add_log(f"Error creating Vacation table: {e}")
