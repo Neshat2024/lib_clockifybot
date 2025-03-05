efa9bd017e8ff5ef479771e075c040b34a75367a
retr0 err0r <retr0err0r@protonmail.com>
2025-03-05 13:12:21 +0330

    expand hours shift

--- lib_clockifybot/config.py
+++ lib_clockifybot/config.py
@@ -36,7 +36,7 @@ PENDING, SELECTED, CONFIRMED_BY, REJECTED_BY = (
     "REJECTED_BY",
 )
 HEADER, SELECT, REMOVE, CONFIRM = "header", "select", "remove", "confirm"
-WHOLE_DAY_HOURS = "8-9-10-11-12-13-14-15-16-17-18-19-20"
+WHOLE_DAY_HOURS = "7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24"
 SHIFT = "shift"
 ok_status_codes = [200, 201]
 cancel = "/cancel"
